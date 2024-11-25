5428d035ddd0b7b4d7056b61a7757c6787498904
pramod kumar <Pramod7017440873@gmail.com>
2024-11-25 11:12:44 +0530

    added indexx.js

---
+++ indexx.js
@@ -0,0 +1,55 @@
+function home(){
+    document.getElementById("homes").style.color="rgb(20,251,20)";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+
+}
+function program(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="rgb(20,251,20)";
+
+}
+function plan(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="rgb(20,251,20)";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+
+}
+function blog(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="white";
+    document.getElementById("blogs").style.color="rgb(20,251,20)";
+    document.getElementById("programs").style.color="white";
+
+}
+function contact(){
+    document.getElementById("homes").style.color="white";
+    document.getElementById("plans").style.color="white";
+    document.getElementById("contacts").style.color="rgb(20,251,20)";
+    document.getElementById("blogs").style.color="white";
+    document.getElementById("programs").style.color="white";
+
+}
+
+//SubMit
+function submit(){
+    let name=document.getElementById("name");
+    let number=document.getElementById("number");
+    if(name.value==""){
+        alert(" Please Enter your Name ");
+    }
+    else if(number.value==""){
+        alert("Please enter your Number")
+    }
+    else{
+        alert("Thanks For Join:"+name.value);
+    }
+}
